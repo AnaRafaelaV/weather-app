57b10f26a6a199fcf5bafce67416f897d2bb2430
AnaRafaelaV <36711747+AnaRafaelaV@users.noreply.github.com>
2022-02-01 19:21:26 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   url: 'http://api.openweathermap.org/data/2.5/weather',
   urlForecaste: 'https://api.openweathermap.org/data/2.5/onecall',
-  apiKey: 'bb30418cee547e8efec69b86e786c708'
+  apiKey: '' //open weather api key
 };
